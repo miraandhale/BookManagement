2cef006422fea57a832a7d2f8a48a7cdb4dafbe6
miraandhale <meeraga22@gmail.com>
2022-02-17 12:19:52 +0530

    Book Entity Added

---
+++ src/main/java/com/mira/bookstore/Book.java
@@ -0,0 +1,52 @@
+package com.mira.bookstore;
+
+import org.graalvm.compiler.replacements.nodes.CStringConstant;
+
+public class Book {
+    protected int id;
+    protected String title;
+    protected String author;
+    protected float price;
+
+    public Book() {
+    }
+    public Book(int id, String title, String author, float price){
+        super();
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
