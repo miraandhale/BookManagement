128edbd115f4e3f81692390e29554243c2fcd1bc
miraandhale <meeraga22@gmail.com>
2022-02-17 13:41:37 +0530

    Book entity edited

--- src/main/java/com/mira/bookstore/Book.java
+++ src/main/java/com/mira/bookstore/Book.java
@@ -10,9 +10,17 @@ public class Book {
 
     public Book() {
     }
-    public Book(int id, String title, String author, float price){
-        super();
+
+    public Book(int id) {
         this.id = id;
+    }
+
+    public Book(int id, String title, String author, float price) {
+        this(title, author, price);
+        this.id = id;
+    }
+
+    public Book(String title, String author, float price) {
         this.title = title;
         this.author = author;
         this.price = price;
